0c8bc6e4dccfea4434f5edb2c6bdf0a64dac1179
Josef Strobel <josef@strobel.eu>
2017-02-23 17:34:22 +0100

    fixing tws and gateway loading scripts

--- xpra.attach.ssh.sh
+++ xpra.attach.ssh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 . ib-controller.config
-xpra attach --bell=no --cursors=no --keyboard-sync=no --notifications=no --clipboard=no --opengl=no --ssh="ssh -o StrictHostKeyChecking=no" ssh/root@127.0.0.1:$SSH_PORT/100
-#xpra --ssh="ssh -o StrictHostKeyChecking=no" ssh/root@127.0.0.1:$SSH_PORT/100
+xpra attach --ssh="ssh -o StrictHostKeyChecking=no" ssh/root@127.0.0.1:$SSH_PORT/100
